c882f98be94862afa2cce641b74321f766355c46
pepitoternera <xosegq16@gmail.com>
2021-11-13 11:24:59 +0100

    second comit: trying some typing doubts and how to build a generator

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print('hello world')
+#typing return multiple
+from typing import Tuple, Generator
+
+def prueba(a: int, b: int, c: float) -> Tuple[int,int,float]:
+    return a,b,c
+
+#funcion generadora con yield
+def f_gen(lista_cosas: tuple) -> Generator:
+    for i in lista_cosas:
+        yield i
+
+cosas = f_gen(prueba(1,2,3.4))
+for i in cosas:
+    print(i)
